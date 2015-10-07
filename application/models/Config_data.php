d253c3810a6cd8fc6b3870ecfdbda0f5270433ce
Ismael Gaudioso <ismael@bacubacu.com>
2015-10-07 14:35:49 +0200

    Corregir bug minimo

--- application/models/Config_data.php
+++ application/models/Config_data.php
@@ -49,7 +49,7 @@ class Config_data extends CI_Model {
 	public function updateIp($ip)
 	{
 		$update = "UPDATE config SET config_value=".$ip." WHERE config_id=1";
-		$query_result = $this->db->query($sql);
+		$query_result = $this->db->query($update);
 	}
 
 }
