41a5f4864181c68ebc8c3c39c8688142f0c8157d
Ismael Gaudioso <ismael@bacubacu.com>
2015-10-07 12:10:28 +0200

    Corregido bug del modelo de datos

--- application/models/data_model.php
+++ application/models/data_model.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Data_model extends CI_Model {
 
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
 	public function getTestData()
 	{
 		return $this->db->get("config")->result();
